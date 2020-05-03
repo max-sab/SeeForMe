5994b9264bba47036bc7ee78833147a73b0c8bab
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2020-05-03 23:23:16 +0300

    project structure reorganized, new VCs added

---
+++ IDentify/ViewControllers/SavedTextsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SavedTextsViewController.swift
+//  IDentify
+//
+//  Created by Maksym Sabadyshyn on 5/3/20.
+//  Copyright © 2020 Maksym Sabadyshyn. All rights reserved.
+//
+
+import UIKit
+
+class SavedTextsViewController: UIViewController {
+
+}
