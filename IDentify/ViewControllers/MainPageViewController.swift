f3ec1b0db8887175e6b4e76dca03be53874b0573
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2020-05-04 21:50:00 +0300

    Design remaking finished

--- IDentify/ViewControllers/MainPageViewController.swift
+++ IDentify/ViewControllers/MainPageViewController.swift
@@ -7,13 +7,31 @@
 //
 
 import UIKit
-
 class MainPageViewController: UIViewController {
     
     @IBOutlet weak var actionButton: UIButton!
     var buttonName: String?
     var buttonIndex: Int!
 
+    private let voiceController = VoiceController()
+
+
+    @IBAction func actionButtonPressed(_ sender: UIButton) {
+        var vc = UIViewController()
+        switch buttonName {
+        case "Read text":
+            vc = self.storyboard?.instantiateViewController(withIdentifier: "VisionViewController") as! VisionViewController
+        case "Identify color":
+            vc = self.storyboard?.instantiateViewController(withIdentifier: "ColorViewController") as! ColorViewController
+        case "Saved texts":
+            vc = self.storyboard?.instantiateViewController(withIdentifier: "SavedTextsViewController") as! SavedTextsViewController
+        case "Saved colors":
+            vc = self.storyboard?.instantiateViewController(withIdentifier: "SavedColorsViewController") as! SavedColorsViewController
+        default:
+            voiceController.read(text: "Unexpected error occured. Please, restart an app")
+            self.present(vc, animated: false, completion: nil)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
